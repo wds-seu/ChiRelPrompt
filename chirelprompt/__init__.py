7218508c118e5f0bf55854b55153b7763983cced
CaiXinyu <68982496+cxy1122@users.noreply.github.com>
2023-04-21 11:35:25 +0800

    Update __init__.py

--- chirelprompt/__init__.py
+++ chirelprompt/__init__.py
@@ -1,1 +1,6 @@
+from . import prompt, evaluator
+from .data import get_training_data
+from .model import Dataset, EncodePlus, ChiRelPrompt
+from .trainer import Trainer
+from .util import cosine_similarity, euclidean_distance
 
